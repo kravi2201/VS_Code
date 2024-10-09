13fd1e676cc59592d12763147f803d613750909e
Avinash Kumar <147703363+kravi2201@users.noreply.github.com>
2024-10-09 11:57:39 +0530

    Add files via upload

---
+++ Fibbnocci.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+int fibbonacci(int n){
+    if(n==0 || n==1){
+        return 1;
+    }
+    else {
+        return fibbonacci(n-1)+fibbonacci(n-2);
+}}
+int main(){
+    int number;
+    cin>>number;
+    // int series=
+    for(int i=0;i<number;i++){
+    cout<<"\t"<<fibbonacci(i);
+    }
+
+    return 0;
+}
